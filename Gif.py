786ff099480d8d0e7a93e749b33288118ddc48ba
kalincammrina <78634912+kalincammrina@users.noreply.github.com>
2025-09-17 11:54:22 +0530

    Update Gif.py

--- Gif.py
+++ Gif.py
@@ -1,14 +1,22 @@
+# Import required libraries
+# - imageio.v3 (aliased as iio) for reading and writing images
+# - os for working with file paths and directories
+
 import imageio.v3 as iio
 
-image_folder = './images'
-
+# A list to hold the loaded images
 filenames = ['GIF/chicklet1.png', 'GIF/chicklet2.png', 'GIF/chicklet3.png', 'GIF/chicklet4.png']
-images =  './images'
+images = [ ]
 
+# Loop through each filename, load the image, and add it to the list
 for filename in filenames:
   images.append(iio.imread(filename))
 
-iio.imwrite('GIF/Chick.gif', images, duration = 500, loop = 0)
+# Save the images as GIF
+# - duration = 600 → each frame is displayed for 600 ms
+# - loop=0 → the GIF repeats forever
+
+iio.imwrite('GIF/Chick.gif', images, duration = 600, loop = 0)
 
 
 
